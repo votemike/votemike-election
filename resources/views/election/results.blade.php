ff4e5649e3a48e5ad9777a7ca8aa559315b007c8
Michael Gwynne <michael.gwynne@C02NV321G3QT.home>
2015-02-16 17:33:48 +0000

    Adding Tweet button

--- resources/views/election/results.blade.php
+++ resources/views/election/results.blade.php
@@ -7,5 +7,8 @@
 		<p>{{ $result->party->name }} - You were shown <strong>{{ $result->shown }}</strong> {{ $result->party->name }} {{ $result->shown == 1 ? 'policy' : 'policies' }} and selected <strong>{{ $result->for }}</strong> of them.</p>
 	@endforeach
 
+	<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="{{ url('election') }}" data-text="This page reckons I should vote for {{ $partyResults[0]->party->name }}. What about you?" data-size="large" data-count="none" data-hashtags="ge2015,ukelection2015,ukge2015">Tweet your result</a></p>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
 	<a href="{{ url('election') }}">Try again</a>
 @endsection
